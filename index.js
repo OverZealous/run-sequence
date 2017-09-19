c450122365b3342eca6c4853de750ed5f1091687
Phil DeJarnett <phil@overzealous.com>
2017-09-19 09:02:45 -0400

    Changelog, cleanup for orchestration events change

--- index.js
+++ index.js
@@ -104,14 +104,14 @@ function runSequence(gulp) {
 
 	function onGulpError(e) {
 		// In the case that you call gulp.stop after a successful run,
-		// we will not recieve a task_err or task_stop event. This callback
+		// we will not receive a task_err or task_stop event. This callback
 		// will finish the run sequence execution in case of an 'orchestration aborted'
 		// even coming from gulp's global error handler. That event is fired in when
 		// gulp.stop is called.
 		if(e.message === 'orchestration aborted') {
 			finish(e);
 		}
-	};
+	}
 
 	function runNextSet() {
 		if(taskSets.length) {
